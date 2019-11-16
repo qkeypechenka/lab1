823e0513a5ae00a85170a3bbc2bf95eff5847805
qkey <petrovsergy@outlook.com>
2019-11-16 16:20:00 +0500

    use enums and states

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -6,11 +6,19 @@
 #define G_OUT 7 
 #define B_OUT 8
 
-#define PIN_BUTTON 5
+#define PIN_BUTTON 2
 
-#define RED "red"
-#define BLUE "blue"
-#define GREEN "green"
+enum Color {
+  RED,
+  BLUE,
+  GREEN
+};
+
+enum State {
+  Blink,
+  ShowColor
+};
+
 Button button = Button(PIN_BUTTON);
 
 void setup() {
@@ -20,33 +28,48 @@ void setup() {
 }
 
 void loop() {
-    if (button.wasPressed()) {   
-        show();
-    }
+  if (button.wasPressed()) {   
+      show();
+  }
 }
 
 void show() {
+  State state = ShowColor;
   updateColor(RED);
   unsigned long startTime = millis();
   while (1) {
     unsigned long timePassed = millis() - startTime;
     if (timePassed % 5000 == 0) {
+      state = Blink;
       makeBlink();
-      updateColor(RED);
     }
-    if (timePassed > 30000) {
-      updateColor(BLUE);
+    
+    switch (state) {
+      case Blink:
+        if (timePassed % 500 == 0) {
+          state = ShowColor;
+        }
+      case ShowColor:
+        if (timePassed <= 30000) {
+          updateColor(RED);
+        } else {
+          updateColor(BLUE);
+        }
     }
   }
 }
 
-void updateColor(String color) {
-  if (color == RED) {
-    set_rgb_led(255, 0, 0);
-  } else if (color == GREEN) {
-    set_rgb_led(0, 255, 0);
-  } else if (color == BLUE) {
-    set_rgb_led(0, 0, 255);
+void updateColor(Color color) {
+  switch (color) {
+    case RED:
+      set_rgb_led(255, 0, 0);
+      break;
+    case GREEN:
+      set_rgb_led(0, 255, 0);
+      break;
+    case BLUE:
+      set_rgb_led(0, 0, 255);
+      break;
   }
 }
 
